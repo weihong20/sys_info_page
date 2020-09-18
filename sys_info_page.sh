11c91425823776da4d6a31b8c94926bc2d56ef19
Weihong Gu <weihonggu@Weihongs-MacBook-Air.local>
2020-09-17 22:22:19 -0400

    adding function stubs

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -5,6 +5,16 @@ TITTLE="System Information Report for $HOSTNAME"
 CURRENT_TIME="$(date +"%x %r %Z")"
 TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
+report_uptime () {
+  return
+}
+report_disk_space () {
+  return
+}
+report_home_space () {
+  return
+}
+
 cat << _EOF_
 <html>
 <head>
@@ -13,6 +23,9 @@ cat << _EOF_
 <body>
 <h1>$TITTLE</h1>
 <p>$TIMESTAMP</p>
+$(report_uptime)
+$(report_disk_space)
+$(report_home_space)
 </body>
 </html>
 _EOF_
