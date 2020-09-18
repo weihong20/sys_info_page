b643f3573450d94ee2d21ba4269bfe3bc624dde2
Weihong Gu <weihonggu@Weihongs-MacBook-Air.local>
2020-09-17 22:01:18 -0400

    add some data

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -2,13 +2,17 @@
 
 # Program to output a system information page
 TITTLE="System Information Report for $HOSTNAME"
+CURRENT_TIME="$(date +"%x %r %Z")"
+TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
-# Program to output a system information page
-echo "<html>
+cat << _EOF_
+<html>
 <head>
 <title>$TITTLE</title>
 </head>
 <body>
 <h1>$TITTLE</h1>
+<p>$TIMESTAMP</p>
 </body>
-</html>"
+</html>
+_EOF_
