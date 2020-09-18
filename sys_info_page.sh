4d3c5fe5b999d6700e5ba06b66b56b5567548ff9
Weihong Gu <weihonggu@Weihongs-MacBook-Air.local>
2020-09-17 22:28:37 -0400

    Flesh out functions

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -6,12 +6,15 @@ CURRENT_TIME="$(date +"%x %r %Z")"
 TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
 report_uptime () {
+echo "Function report_uptime executed."
   return
 }
 report_disk_space () {
+echo "Function report_disk_space executed."
   return
 }
 report_home_space () {
+echo "Function report_home_space executed."
   return
 }
 
