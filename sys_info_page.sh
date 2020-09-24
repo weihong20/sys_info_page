eec4aa79f9664f7b3bb4322170ef96c1d740e191
Weihong Gu <guweihong@login4.ufhpc>
2020-09-23 20:47:58 -0400

    adding variables

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # Program to output a system information page
+
+title="System Information Report"
+
 echo "<html>
    <head>
-        <title>System Information Report</title>
+        <title>$title</title>
    </head>
    <body>
-        <h1>System Information Report</h1>
+        <h1>$title</h1>
    </body>
 </html>"
