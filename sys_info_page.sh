3a73b0f076769d0eaae45cc1685f8627b0ed1c1e
Weihong Gu <guweihong@login4.ufhpc>
2020-09-23 20:15:10 -0400

    first version of the script

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-
 # Program to output a system information page
-TITTLE="System Information Report for $HOSTNAME"
-CURRENT_TIME="$(date +"%x %r %Z")"
-TIMESTAMP="Generated $CURRENT_TIME, by $USER"
-
-cat << _EOF_
-<html>
-<head>
-<title>$TITTLE</title>
-</head>
-<body>
-<h1>$TITTLE</h1>
-<p>$TIMESTAMP</p>
-</body>
-</html>
-_EOF_
+echo "<html>
+   <head>
+        <title>Page Title</title>
+   </head>
+   <body>
+        Page body.
+   </body>
+</html>"
